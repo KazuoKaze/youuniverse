3b8f60fd28a4b8750d232d72ea6f55aafdbf1a2c
Sourav Mehta <kazuokaze00@gmail.com>
2026-03-25 16:17:58 +0530

    Changes config file

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,52 @@
-import { withPayload } from '@payloadcms/next/withPayload'
-import type { NextConfig } from 'next'
-import path from 'path'
-import { fileURLToPath } from 'url'
+// import { withPayload } from '@payloadcms/next/withPayload'
+// import type { NextConfig } from 'next'
+// import path from 'path'
+// import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const dirname = path.dirname(__filename)
+
+// const nextConfig: NextConfig = {
+//   webpack: (webpackConfig) => {
+//     webpackConfig.resolve.extensionAlias = {
+//       '.cjs': ['.cts', '.cjs'],
+//       '.js': ['.ts', '.tsx', '.js', '.jsx'],
+//       '.mjs': ['.mts', '.mjs'],
+//     }
+
+//     return webpackConfig
+//   },
+//   turbopack: {
+//     root: path.resolve(dirname),
+//   },
+// }
+
+// export default withPayload(nextConfig, { devBundleServerPackages: false })
+
+import type { NextConfig } from 'next'
+import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // ✅ Required for Payload
+  serverExternalPackages: ['payload', 'pino', 'thread-stream'],
+
+  // Required because Payload modifies webpack
+  turbopack: {},
+
+  webpack: (config) => {
+    config.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
-    return webpackConfig
-  },
-  turbopack: {
-    root: path.resolve(dirname),
+    return config
   },
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, {
+  devBundleServerPackages: false,
+})
